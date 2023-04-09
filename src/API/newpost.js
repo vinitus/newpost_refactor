969441f7f6619bde6e1e55ecade8291e9f45e434
vinitus <tlsdnrng@gmail.com>
2023-04-09 20:49:44 +0900

    [feat] API request function

---
+++ src/API/newpost.js
@@ -0,0 +1,23 @@
+export default async function newpostAPI(
+  recipeCreateReq,
+  thumbnailImgFile,
+  stepImgFile,
+  token
+) {
+  console.log(recipeCreateReq);
+  const data = new FormData();
+  data.append(
+    "recipeCreateReq",
+    new Blob([JSON.stringify(recipeCreateReq)], { type: "application/json" })
+  );
+  data.append("file", thumbnailImgFile);
+  stepImgFile.forEach((file) => {
+    if (file) {
+      data.append("file", file);
+    }
+  });
+
+  for (const value of data) {
+    console.log(value);
+  }
+}
