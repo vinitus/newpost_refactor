d07e7133995966a2a1a40ee735adae674201d91b
vinitus <tlsdnrng@gmail.com>
2023-04-14 09:29:26 +0900

    eslint setting

--- src/API/newpost.js
+++ src/API/newpost.js
@@ -1,19 +1,14 @@
-export default async function newpostAPI(
-  recipeCreateReq,
-  thumbnailImgFile,
-  stepImgFile,
-  token
-) {
+export default async function newpostAPI(recipeCreateReq, thumbnailImgFile, stepImgFile, token) {
   console.log(recipeCreateReq);
+
   const data = new FormData();
-  data.append(
-    "recipeCreateReq",
-    new Blob([JSON.stringify(recipeCreateReq)], { type: "application/json" })
-  );
-  data.append("file", thumbnailImgFile);
+
+  data.append('recipeCreateReq', new Blob([JSON.stringify(recipeCreateReq)], { type: 'application/json' }));
+  data.append('file', thumbnailImgFile);
+
   stepImgFile.forEach((file) => {
     if (file) {
-      data.append("file", file);
+      data.append('file', file);
     }
   });
 
