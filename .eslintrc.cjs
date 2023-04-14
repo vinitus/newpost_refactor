0a4722f23a00401cc4c44c256522768ddb824989
vinitus <tlsdnrng@gmail.com>
2023-04-14 10:37:42 +0900

    refactoring start!

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,45 +1,8 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
   },
-  extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
-  rules: {
-    "linebreak-style": 0,
-    "import/prefer-default-export": 0,
-    "prettier/prettier": 0,
-    "import/extensions": 0,
-    "no-use-before-define": 0,
-    "import/no-unresolved": 0,
-    "import/no-extraneous-dependencies": 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
-    "no-shadow": 0,
-    "react/prop-types": 0,
-    "react/jsx-filename-extension": [
-      2,
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-    ],
-    "jsx-a11y/no-noninteractive-element-interactions": 0,
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  plugins: [],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,50 +15,39 @@ const px0To200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
 // const px0To1300 = { ...Array.from(Array(1301)).map((_, i) => `${i}px`) };
 const px0To1400 = { ...Array.from(Array(1401)).map((_, i) => `${i}px`) };
 
-module.exports = {
-  content: [
-    "./src/Components/*.{html,js,jsx}",
-    // "./src/Components/auth/*.{html,js,jsx}",
-    "./src/Pages/*.{html,js,jsx}",
-    "./src/UI/*.{html,js,jsx}",
-    "./src//*.{html,js,jsx}",
-    "./src/*.{html,js,jsx}",
-    "./index.html",
-    "./node_modules/tw-elements/dist/js/**/*.js",
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        noto: "Noto Sans",
+        noto: 'Noto Sans',
         // nanum: "Nanum Square",
-        sans: "Nanum Square",
+        sans: 'Nanum Square',
       },
       spacing: {
         ...px0To1400,
       },
       colors: {
-        prettywhite: "#FDFDFD",
-        DEFAULT: "#212121",
-        "#7F807F": "#7F807F",
-        "#ECECEC": "#ECECEC",
-        "#2F80ED": "#2F80ED",
-        "#AEAFAE": "#AEAFAE",
-        "#F5F5F5": "#F5F5F5",
-        "#FFFFFB": "#FFFFFB",
-        "#535453": "#535453",
-        "#B3B3B3": "#B3B3B3",
-        "#2B2C2B": "#2B2C2B",
-        "#F2F2F2": "#F2F2F2",
-        "#FF6B6C": "#FF6B6C",
-        "#FFD1D1": "#FFD1D1",
+        prettywhite: '#FDFDFD',
+        DEFAULT: '#212121',
+        '#7F807F': '#7F807F',
+        '#ECECEC': '#ECECEC',
+        '#2F80ED': '#2F80ED',
+        '#AEAFAE': '#AEAFAE',
+        '#F5F5F5': '#F5F5F5',
+        '#FFFFFB': '#FFFFFB',
+        '#535453': '#535453',
+        '#B3B3B3': '#B3B3B3',
+        '#2B2C2B': '#2B2C2B',
+        '#F2F2F2': '#F2F2F2',
+        '#FF6B6C': '#FF6B6C',
+        '#FFD1D1': '#FFD1D1',
       },
       fontSize: px0To200,
       lineHeight: px0To100,
       borderRadius: px0To100,
     },
   },
-  variants: {
-    extend: {},
-  },
+
   plugins: [],
 };
